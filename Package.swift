d0db81aff6e65a342bc87246fc080896cf52f31f
Trulioo Release Automation <release-automation@trulioo.com>
2026-04-27 09:31:36 -0700

    Release 3.0.0-beta.2

--- Package.swift
+++ Package.swift
@@ -2,10 +2,10 @@
 import PackageDescription
 
 // Updated by scripts/package_binary_artifact.sh during release preparation.
-let truliooArtifactURL = "https://github.com/Trulioo/trulioo-ios/releases/download/3.0.0-beta.1/Trulioo.xcframework.zip"
-let truliooArtifactChecksum = "19bbddf774266d11e82ba47dab4aff438602350b9263bb9a73d1b68e98fad93b"
-let truliooCoreArtifactURL = "https://github.com/Trulioo/trulioo-ios/releases/download/3.0.0-beta.1/TruliooCore.xcframework.zip"
-let truliooCoreArtifactChecksum = "08faf803ce18e4bb2debaab7cb123d9e97c10afcbb99b98d5e447e11323c6f81"
+let truliooArtifactURL = "https://github.com/Trulioo/trulioo-ios/releases/download/3.0.0-beta.2/Trulioo.xcframework.zip"
+let truliooArtifactChecksum = "4d9a1209ef5d2c4b0a4c751cc5464c1b17d03554bcc0161c9e328c0237902f14"
+let truliooCoreArtifactURL = "https://github.com/Trulioo/trulioo-ios/releases/download/3.0.0-beta.2/TruliooCore.xcframework.zip"
+let truliooCoreArtifactChecksum = "458b9b374644db0c76cdd2dfaedd6f5437c1799b6c8ae6f94264f83880670c87"
 
 let package = Package(
     name: "Trulioo",
